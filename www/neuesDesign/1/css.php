98b044b20a30a9833114c15931471cf432f2bafe
Sebastian <sickelcoding@googlemail.com>
2013-04-02 12:25:44 +0200

    remove www. prepare submodule

--- www/neuesDesign/1/css.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'default.php';
-header('Content-Type: text/css');
-
-$smarty->left_delimiter = 'MitDiesenDelimeterFaengtHierNixAn{';
-$smarty->right_delimiter = '}MitDiesenDelimeterHoertHierNixAuf';
-$smarty->display('css.tpl');
-?>
